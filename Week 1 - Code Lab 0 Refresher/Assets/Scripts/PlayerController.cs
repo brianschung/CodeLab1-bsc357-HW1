90c504c436630a9f6bf99f9020a89a01d3f47b06
Brian <briansangilchung@gmail.com>
2020-02-03 15:48:58 -0500

    added initial features

---
+++ Week 1 - Code Lab 0 Refresher/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	
+	public float force;
+	Rigidbody2D rb;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world.");
+		rb = GetComponent<Rigidbody2D>();
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("");
+		
+		if (Input.GetKey(KeyCode.W)) // if W is pressed
+		{
+			rb.AddForce(Vector2.up * force); //apply a force using the "force" var
+		}
+		if (Input.GetKey(KeyCode.A)) // if A is pressed
+		{
+			rb.AddForce(Vector2.left * force); //apply a force using the "force" var
+		}
+		if (Input.GetKey(KeyCode.D)) // if D is pressed
+		{
+			rb.AddForce(Vector2.right * force); //apply a force using the "force" var
+		}
+		if (Input.GetKey(KeyCode.S)) // if S is pressed
+		{
+			rb.AddForce(Vector2.down * force); //apply a force using the "force" var
+		}
+		
+
+	}
+}
