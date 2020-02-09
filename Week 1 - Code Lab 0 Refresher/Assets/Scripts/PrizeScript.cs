96f0638c55284a2392119431fb285512fa5694f8
Brian <briansangilchung@gmail.com>
2020-02-09 14:09:13 -0500

    model, prize, update controls

--- Week 1 - Code Lab 0 Refresher/Assets/Scripts/PrizeScript.cs
+++ Week 1 - Code Lab 0 Refresher/Assets/Scripts/PrizeScript.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class PrizeScript : MonoBehaviour
 {
-	
+
+    private Rigidbody2D rb;
+
+    public GameObject myPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+            
     }
 
-    // Update is called once per frame
-    void Update()
+            // Update is called once per frame
+            void Update()
     {
+        if (this.transform.position.y < -80)
+        {
+            //transform.position = new Vector2(Random.Range(-3, 3), Random.Range(10,11));
+            //rb = gameObject.GetComponent<Rigidbody2D>();
+            //rb.velocity = new Vector2(0.0f, 0.0f);
+            Instantiate(myPrefab, new Vector2(Random.Range(-3, 3), Random.Range(20, 22)), Quaternion.identity);
+            Destroy(gameObject);
+        }
 
     }
 	
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-		transform.position = new Vector2(Random.Range(-3,3), Random.Range(-3,3));
+        //transform.position = new Vector2(Random.Range(-3,3), Random.Range(20,22));
+        Instantiate(myPrefab, new Vector2(Random.Range(-3, 3), Random.Range(20, 22)), Quaternion.identity);
+        Destroy(gameObject);
+        Model.score = Model.score+1;
+        Debug.Log(Model.score);
 	}
 
 }
